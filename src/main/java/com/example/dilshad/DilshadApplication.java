0d22b2a07872902827b3c834d4453635ee676515
Mohammad <Dilshad.Mohammad@mnscorp.net>
2021-10-11 04:03:00 +0000

    initial Commit

---
+++ src/main/java/com/example/dilshad/DilshadApplication.java
@@ -0,0 +1,13 @@
+package com.example.dilshad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DilshadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DilshadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dilshad/DilshadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dilshad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DilshadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
